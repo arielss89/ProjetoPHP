71df9214e08cae5a543995357593eec0af955018
arielss89 <arielsoares89@gmail.com>
2018-10-04 19:32:50 -0300

    Envio do projeto de php, primeira versão

---
+++ rodape.php
@@ -0,0 +1,24 @@
+<?php
+echo '
+<footer class="py-1 page-footer font-small blue pt-4 footer">
+    <div class="container-fluid text-center text-md-left">
+      <div class="row">
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="#"> Ariel Soares da Silva</a>
+    </div>
+    </footer>';
+
+
+
+
+?>
+<!--
+<style>
+.footer {
+    clear: both;
+    position: relative;
+    z-index: 10;
+    height: 3em;
+    margin-top: -3em;
+}
+</style>-->
